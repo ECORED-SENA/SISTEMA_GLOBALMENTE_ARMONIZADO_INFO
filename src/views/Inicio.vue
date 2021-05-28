3263c9cf992d74951492d3b0117d4a69a4b06970
Luis Gabriel Urueta Alvarez <lgurueta@misena.edu.co>
2021-05-28 10:34:12 -0500

    Maquetación v1

---
+++ src/views/Inicio.vue
@@ -0,0 +1,96 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p.mb-4 En la vida moderna el uso de productos químicos en las actividades cotidianas es común. La mayoría de las personas que en las empresas de diversos sectores usa productos químicos, desconoce los peligros que estos generan no solo a ellas, sino al ambiente y a la infraestructura. Esta es una situación creciente, teniendo en cuenta que en el año 2015 el registro mundial de productos químicos superó los 100 millones y al año en el mundo se producen al menos 400 millones de toneladas de productos químicos para la agricultura, alimentos, combustibles, aseo, cosmético, fármacos, entre otros.
+    p.mb-4 En Colombia el personal que manipula, almacena, transporta internamente y maneja los residuos de los productos químicos, conoce poco sobre sus efectos agudos y crónicos, según su tipología para las personas expuestas y, adicionalmente, se trabaja con pocas medidas de prevención y protección. El desconocimiento en el manejo seguro de productos químicos se evidencia en los diferentes niveles de las organizaciones, desde la dirección en la toma de decisiones hasta las actividades meramente operativas en las que se deberían implementar dichas decisiones.
+    p.mb-4 Por todo lo anterior, es el Estado, los empleadores y los trabajadores quienes han buscado medidas de acción que aseguren el buen uso de los productos químicos y, de esta forma, minimizar las afecciones a la salud, a la seguridad y al medio ambiente sin descuidar la productividad. Para ello se han concretado diferentes iniciativas como la implementación del Decreto 1496 del 06 de agosto de 2018, el Pacto Sectorial para la Cadena de Químicos y el Conpes 3868 de 2016 - Política de gestión del riesgo asociado al uso de productos químicos, que propone el curso enfocado al cierre de brechas en la industria en la aplicación del Sistema Globalmente Armonizado de clasificación y etiquetado de productos químicos (SGA) para la adecuada gestión de los productos químicos.
+    p El programa de formación complementaria virtual en Aplicación del Sistema Globalmente Armonizado (SGA) en el manejo, almacenamiento, transporte interno y residuos de productos químicos, brinda herramientas para que los usuarios de productos químicos desarrollen estrategias territoriales dirigidas al adecuado manejo, almacenamiento y transporte interno de residuos de los productos químicos minimizando los impactos no deseados generados por estos.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación del programa
+
+    p.mb-4 El uso de productos químicos en las actividades cotidianas se ha vuelto cada vez más común, pues la mayoría de las empresas de diferentes sectores, incluyendo el educativo, usa productos químicos. En Colombia muchos trabajadores y estudiantes deben manipular productos químicos en el desempeño de sus funciones, sin embargo, los usuarios de productos químicos conocen muy poco sobre las consecuencias, agudas o crónicas a las que están expuestos, razón por la cual se trabaja con insuficientes o nulas medidas de prevención y protección. El desconocimiento en el manejo seguro de productos químicos incide sobre todos los niveles de las organizaciones, desde la dirección en la toma de decisiones hasta las actividades meramente operativas en las que se deberían implementar dichas decisiones.
+    p.mb-4 La primera línea en la protección de la salud, la seguridad y el medio ambiente contra el riesgo químico es el conocimiento de las propiedades de los productos químicos utilizadas. En algunos casos, los riesgos están comunicados por el proveedor o fabricante, pero en otros no hay comunicación; sin embargo, se debe considerar que dicha comunicación sea interpretada apropiadamente, ya que de esta depende la toma de decisiones, la implementación de medidas de control, el uso racional y la disposición de residuos. 
+    p.mb-4 Históricamente, en el mundo se han diseñado diferentes metodologías comunicativas de los riesgos de los productos químicos, pues cada sistema de comunicación responde a una necesidad sentida en el ciclo de vida de los productos químicos. Se han usado sistemas de comunicación para la intervención segura de sustancias durante emergencias, otros sistemas consideran la compatibilidad durante el transporte interno y almacenamiento, etc. Y la disponibilidad de varios sistemas de comunicación ha generado confusión y en muchos casos mal uso de ellos, por eso, en el año 2002 la ONU propuso al mundo un sistema de comunicación que cubriera todas las necesidades de manera sencilla conocido como Sistema Globalmente Armonizado de rotulación y etiquetado de productos químicos. 
+    p Teniendo como precedente la implementación del Decreto 1496 del 06 de agosto de 2018, el Pacto Sectorial para la Cadena de Químicos ratificado por el Presidente y en el Conpes 3868 de 2016 - Política de gestión del riesgo asociado al uso de productos químicos, se propone el desarrollo de un curso enfocado al cierre de brechas que tiene la industria en la aplicación del Sistema Globalmente Armonizado (SGA) de clasificación y etiquetado de productos químicos para la adecuada gestión de los productos químicos.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Población objetivo
+
+    p Dirigido al personal que labora en el sector industrial, educativo y aprendices que en el desarrollo de sus labores realiza la manipulación, almacenamiento, transporte interno o manejo de los residuos de productos químicos.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Estrategia metodológica
+
+    p.mb-4 Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+
+    p.mb-4 Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que se logran a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+
+    .row.g-4.justify-content-center
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center
+          strong Las TIC
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/04.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El trabajo colaborativo
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
